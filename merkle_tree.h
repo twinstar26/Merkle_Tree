e4f14406d213604ec75f46c94cc9b9e3a09d4c97
twinstar26 <adveer_b17@it.vjti.ac.in>
2019-08-15 11:05:48 +0530

    devified md5 suite

--- merkle_tree.h
+++ merkle_tree.h
@@ -1,18 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
 #include "crypto/sha1.h"
-#include<string>
-#include<vector>
-#include<iostream>
 
-class Node
-{
-public:
-    std::string hash;
-    std::vector<class Node*> child;
+class Node {
+ public:
+  std::string hash;
+  std::vector<class Node*> child;
 
-
-    Node(std::string init = "")
-    {
-        this->hash = init;
-    }
+  Node(std::string init = "") { this->hash = init; }
 };
 
+static void MDPrint(unsigned char digest[16]) {
+  unsigned int i;
+
+  for (i = 0; i < 16; i++) printf("%02x", digest[i]);
+}
+
+static unsigned char* MDString(unsigned char* string) {
+  MD5_CTX context;
+  unsigned char digest[16];
+  unsigned int len = strlen(reinterpret_cast<const char*>(string));
+
+  MD5Init(&context);
+  MD5Update(&context, string, len);
+  MD5Final(digest, &context);
+  return digest;
+}
