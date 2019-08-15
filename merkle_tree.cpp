e0e7005bc31624a3173572abd68e878cb9de9165
twinstar26 <adveer_b17@it.vjti.ac.in>
2019-08-15 11:59:39 +0530

    version1 functions

--- merkle_tree.cpp
+++ merkle_tree.cpp
@@ -1,34 +1,40 @@
 #include "merkle_tree.h"
-#include "crypto/md5c.h"
+#include <cstring>
+#include<string>
 
-static void MDString (char *string)
-{
-  MD5_CTX context;
-  unsigned char digest[16];
-  unsigned int len = strlen (string);
+using namespace std;
 
-  MD5Init (&context);
-  MD5Update (&context, string, len);
-  MD5Final (digest, &context);
+int main() {
+  Node root = Node();
 
-  std::cout<<"md5 is  "<<string<<std::endl;
-  // MDPrint (digest);
-  printf ("\n");
-}
+  Node left = Node("this is left");
+  Node right = Node("this is right");
 
+  root.child.push_back(&left);
+  root.child.push_back(&right);
+
+  root.hash = MDString(left.hash+right.hash);
 
 
-int main()
-{
-    Node root = Node();
+  Node new_root = Node();
 
-    Node left = Node("this is left");
-    Node right = Node("this is right");
+  Node new_left = Node("this is left");
+  Node new_right = Node("this is right");
 
-    root.child.push_back(&left);
-    root.child.push_back(&right);
+  new_root.child.push_back(&new_left);
+  new_root.child.push_back(&new_right);
 
-    char s[] = "hello@6969";
-    MDString(s);
-return 0;
+  new_root.hash = MDString(new_left.hash+new_right.hash);
+
+  if(root.hash==new_root.hash)
+  {
+    cout<<"Successfully implemented Merkle Tree"<<endl;
+    cout<<"Left node's hash: ";MDPrint(left.hash);cout<<" "<<"New Left node's hash: ";MDPrint(new_left.hash);cout<<endl;
+    cout<<"Right node's hash: ";MDPrint(right.hash);cout<<" "<<"New Right node's hash: ";MDPrint(new_right.hash);cout<<endl;
+    cout<<"Root node's hash: ";MDPrint(root.hash);cout<<" "<<"New Root node's hash: ";MDPrint(new_root.hash);cout<<endl;
+  }
+  /* unsigned char *val=new unsigned char[left.hash.length()+1];
+	strcpy((char *)val,left.hash.c_str());
+  MDPrint(val);*/
+  return 0;
 }
--- merkle_tree.h
+++ merkle_tree.h
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <string>
+#include<cstring>
 #include <vector>
-#include "crypto/sha1.h"
+#include "crypto/md5c.h"
+
+static std::string MDString(std::string sstring);
+static void MDPrint(unsigned char digest[16]);
 
 class Node {
  public:
   std::string hash;
   std::vector<class Node*> child;
 
-  Node(std::string init = "") { this->hash = init; }
+  Node(std::string init = "") { this->hash = MDString(init); }
 };
 
-static void MDPrint(unsigned char digest[16]) {
+static void MDPrint(std::string sdigest) {
+  unsigned char *digest=new unsigned char[sdigest.length()+1];
+  strcpy((char *)digest,sdigest.c_str());
+  
   unsigned int i;
 
   for (i = 0; i < 16; i++) printf("%02x", digest[i]);
 }
 
-static unsigned char* MDString(unsigned char* string) {
+static std::string MDString(std::string sstring) {
+  unsigned char *ustring=new unsigned char[sstring.length()+1];
+  strcpy((char *)ustring,sstring.c_str());
+
   MD5_CTX context;
   unsigned char digest[16];
-  unsigned int len = strlen(reinterpret_cast<const char*>(string));
+  unsigned int len = strlen(reinterpret_cast<const char*>(ustring));
 
   MD5Init(&context);
-  MD5Update(&context, string, len);
+  MD5Update(&context, ustring, len);
   MD5Final(digest, &context);
-  return digest;
+  
+  std::string sdigest = reinterpret_cast<char*>(digest);
+  return sdigest;
 }
